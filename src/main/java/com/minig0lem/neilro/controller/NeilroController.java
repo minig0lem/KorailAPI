d68241d6143a30ceceabe1f51b3f9aa9e99a6f7c
minig0lem <gmltjd0326@naver.com>
2024-04-01 06:12:29 +0900

    first commit

---
+++ src/main/java/com/minig0lem/neilro/controller/NeilroController.java
@@ -0,0 +1,32 @@
+package com.minig0lem.neilro.controller;
+
+import com.minig0lem.neilro.constants.StationCode;
+import com.minig0lem.neilro.service.NeilroService;
+import com.minig0lem.neilro.service.OpenApiManager;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.minig0lem.neilro.dto.NeilroDto.NeilroRequest;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+@Slf4j
+public class NeilroController {
+
+    private final NeilroService neilroService;
+    private final OpenApiManager openApiManager;
+
+    @PostMapping("/neilro")
+    public ResponseEntity<Void> findNeilro(@Valid @RequestBody NeilroRequest neilroRequest) {
+        openApiManager.fetch(neilroRequest);
+        return ResponseEntity.ok().build();
+    }
+}
