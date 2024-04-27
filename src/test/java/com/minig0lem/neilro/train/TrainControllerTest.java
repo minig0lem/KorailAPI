556ba134990cea882a340edc2f8bdf9b96a61bca
minig0lem <gmltjd0326@naver.com>
2024-04-28 06:47:05 +0900

    열차 예매 가능 좌석 수 조회 API 구현 완료
    TrainController 테스트 코드 작성 완료
    
    일반승차권 페이지 크롤링 구현
     - 크롬 드라이버 연결 완료
     - 입력 값 기반 열차 시간표 조회 완료
     - 열차 별 총 예매 가능 좌석 수 조회 완료
     - 순방향, 역방향 좌석 계산 완료
    
    오픈 API 수정
     - 사용자 입력 값(출발 시간) 수정

--- src/test/java/com/minig0lem/neilro/train/TrainControllerTest.java
+++ src/test/java/com/minig0lem/neilro/train/TrainControllerTest.java
@@ -17,7 +17,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-//@WebMvcTest(TrainControllerTest.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class TrainControllerTest {
